e9d7961b0dfe339a09ef2082c269a1102fee6887
Mahdi <marsav98@gmail.com>
2024-02-08 00:11:21 +0330

    Initial Nuxt+Tailwind

---
+++ components/commingsoon.vue
@@ -0,0 +1,12 @@
+<template>
+        <div class="relative h-screen w-full flex items-center justify-center bg-cover bg-center text-center px-5"
+            style="background-image:url(https://images.pexels.com/photos/260689/pexels-photo-260689.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=1&amp;w=500);">
+            <div class="absolute top-0 right-0 bottom-0 left-0 bg-gray-900 opacity-75"></div>
+
+            <div class="z-50 flex flex-col justify-center text-white w-full h-screen">
+                <h1 class="text-5xl">We are <b>Almost</b> there!</h1>
+                <p>Stay tuned for something amazing!!!</p>
+                <a href="https://nasirinezhad.ir">nasirinezhad.ir</a>
+            </div>
+        </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="flex justify-center align-content-center">
+        <commingsoon></commingsoon>
+    </div>
+  </template>
+  
